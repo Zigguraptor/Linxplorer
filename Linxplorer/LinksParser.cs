b068d0727e0696b719913c88273ec572536526df
Zigguraptor <cobaltum8@gmail.com>
2023-02-17 06:07:30 +0400

    Add option "--end-of-link="

--- Linxplorer/LinksParser.cs
+++ Linxplorer/LinksParser.cs
@@ -28,9 +28,26 @@ public static class LinksParser
         return new AnalysisResult(parsedFiles.ToArray(), totalLinksCount);
     }
 
+    public static AnalysisResult ExtractLinksByEnd(string[] fieNames, string[] patterns)
+    {
+        if (fieNames == null) throw new ArgumentNullException(nameof(fieNames));
+
+        var totalLinksCount = 0;
+        var parsedFiles = new List<ParsedFile>();
+        foreach (var fieName in fieNames)
+        {
+            if (!File.Exists(fieName)) continue;
+
+            var links = GetLinksFromText(File.ReadAllText(fieName), patterns);
+            parsedFiles.Add(new ParsedFile(fieName, links));
+            totalLinksCount += links.Length;
+        }
+
+        return new AnalysisResult(parsedFiles.ToArray(), totalLinksCount);
+    }
+
     private static Uri[] GetLinksFromText(string text, bool extractLinksOnFiles)
     {
-
         var matches = Regex.Matches(text, LinkPatterns[1]);
         var links = new List<Uri>();
         Uri? uri;
@@ -50,6 +67,22 @@ public static class LinksParser
                 if (Uri.TryCreate(match.ToString(), UriKind.Absolute, out uri))
                     links.Add(uri);
             }
+        }
+
+        return links.ToArray();
+    }
+
+    private static Uri[] GetLinksFromText(string text, string[] patterns)
+    {
+        var matches = Regex.Matches(text, LinkPatterns[1]);
+        var links = new List<Uri>();
+        Uri? uri;
+
+        foreach (Match match in matches)
+        {
+            if (!Uri.TryCreate(match.ToString(), UriKind.Absolute, out uri)) continue;
+            if (patterns.Any(pattern => uri.AbsolutePath.EndsWith(pattern)))
+                links.Add(uri);
         }
 
         return links.ToArray();
